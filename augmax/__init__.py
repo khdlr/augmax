fc9ed5362c98d02dac33955015578e100ce530d4
Konrad Heidler <k.heidler@tum.de>
2021-08-31 15:08:13 +0200

    Add License

--- augmax/__init__.py
+++ augmax/__init__.py
@@ -1,3 +1,18 @@
+# Copyright 2021 Konrad Heidler
+# 
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+# 
+#     http://www.apache.org/licenses/LICENSE-2.0
+# 
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+from .version import __version__
+
 from .base import BaseChain, Transformation, InputType
 
 from .geometric import GeometricTransformation, GeometricChain, HorizontalFlip, VerticalFlip, Rotate, \
@@ -9,3 +24,4 @@ from .colorspace import ColorspaceTransformation, ColorspaceChain, ByteToFloat, 
 from .imagelevel import GridShuffle, Blur, GaussianBlur
 
 from .optimized import Chain, OptimizedChain
+
--- setup.py
+++ setup.py
@@ -1,1 +1,42 @@
-INSTAL_REQUIRES = ["jax>=0.1", "einops>=0.3"]
+# Copyright 2021 Konrad Heidler
+# 
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+# 
+#     http://www.apache.org/licenses/LICENSE-2.0
+# 
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+from setuptools import setup, find_packages
+
+_version_dict = dict()
+exec(open('augmax/version.py').read(), _version_dict)
+__version__ = _version_dict['__version__']
+
+setup(
+    name='augmax',
+    version=__version__,
+    description='Efficiently Composable Data Augmentation on the GPU with Jax',
+    author='Konrad Heidler',
+    packages=find_packages(),
+    python_requires='>=3.7',
+    install_requires = [
+        'jax>=0.1',
+        'einops>=0.3'
+    ],
+    url='https://github.com/khdlr/augmax',
+    license='',
+    classifiers=[
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Science/Research',
+        'License :: OSI Approved :: Apache Software License'
+    ]
+)
+
