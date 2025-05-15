5715d98cab82b67384cd498a167c39fdf758f432
Konrad Heidler <k.heidler@tum.de>
2025-05-15 23:00:10 +0200

    Update intersphinx_mapping for docs.jax.dev

--- docs/conf.py
+++ docs/conf.py
@@ -30,7 +30,7 @@ intersphinx_mapping = {
   "python": ("https://docs.python.org/3/", None),
   "numpy": ("https://numpy.org/doc/stable/", None),
   "scipy": ("https://docs.scipy.org/doc/scipy/reference/", None),
-  "jax": ("https://jax.readthedocs.io/en/latest/", None),
+  "jax": ("https://docs.jax.dev/en/latest/", None),
 }
 
 source_suffix = [".rst", ".ipynb", ".md"]
