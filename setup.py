14423324f5ddfdf329c2f5c90f6c722a95c48035
Konrad Heidler <k.heidler@tum.de>
2025-05-15 22:22:52 +0200

    Switch to uv build backend

--- setup.py
+++ setup.py
@@ -1,11 +1,11 @@
 # Copyright 2024 Konrad Heidler
-# 
+#
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
-# 
+#
 #     http://www.apache.org/licenses/LICENSE-2.0
-# 
+#
 # Unless required by applicable law or agreed to in writing, software
 # distributed under the License is distributed on an "AS IS" BASIS,
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -14,40 +14,35 @@
 from setuptools import setup, find_packages
 
 _version_dict = dict()
-with open('augmax/version.py') as f:
-    exec(f.read(), _version_dict)
-__version__ = _version_dict['__version__']
+with open("augmax/version.py") as f:
+  exec(f.read(), _version_dict)
+__version__ = _version_dict["__version__"]
 
-with open('README.md', 'r', encoding='utf-8') as f:
-    long_description = f.read()
+with open("README.md", "r", encoding="utf-8") as f:
+  long_description = f.read()
 
 setup(
-    name='augmax',
-    version=__version__,
-    description='Efficiently Composable Data Augmentation on the GPU with Jax',
-    author='Konrad Heidler',
-    packages=find_packages(),
-    python_requires='>=3.6',
-    install_requires = [
-        'jax>=0.1',
-        'numpy>=1.22',
-        'einops>=0.3'
-    ],
-    long_description=long_description,
-    long_description_content_type='text/markdown',
-    url='https://github.com/khdlr/augmax',
-    license='',
-    classifiers=[
-        'Programming Language :: Python :: 3.6',
-        'Programming Language :: Python :: 3.7',
-        'Programming Language :: Python :: 3.8',
-        'Programming Language :: Python :: 3.9',
-        'Programming Language :: Python :: 3.10',
-        'Programming Language :: Python :: 3.11',
-        'Programming Language :: Python :: 3.12',
-        'Development Status :: 4 - Beta',
-        'Intended Audience :: Science/Research',
-        'License :: OSI Approved :: Apache Software License'
-    ]
+  name="augmax",
+  version=__version__,
+  description="Efficiently Composable Data Augmentation on the GPU with Jax",
+  author="Konrad Heidler",
+  packages=find_packages(),
+  python_requires=">=3.6",
+  install_requires=["jax>=0.1", "numpy>=1.22", "einops>=0.3"],
+  long_description=long_description,
+  long_description_content_type="text/markdown",
+  url="https://github.com/khdlr/augmax",
+  license="",
+  classifiers=[
+    "Programming Language :: Python :: 3.6",
+    "Programming Language :: Python :: 3.7",
+    "Programming Language :: Python :: 3.8",
+    "Programming Language :: Python :: 3.9",
+    "Programming Language :: Python :: 3.10",
+    "Programming Language :: Python :: 3.11",
+    "Programming Language :: Python :: 3.12",
+    "Development Status :: 4 - Beta",
+    "Intended Audience :: Science/Research",
+    "License :: OSI Approved :: Apache Software License",
+  ],
 )
-
--- src/augmax/__init__.py
+++ src/augmax/__init__.py
@@ -1,28 +1,57 @@
 # Copyright 2024 Konrad Heidler
-# 
+#
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
-# 
+#
 #     http://www.apache.org/licenses/LICENSE-2.0
-# 
+#
 # Unless required by applicable law or agreed to in writing, software
 # distributed under the License is distributed on an "AS IS" BASIS,
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-from .version import __version__
-
 from .base import BaseChain, Transformation, InputType
 
-from .geometric import GeometricTransformation, GeometricChain, HorizontalFlip, VerticalFlip, Rotate, \
-        CenterCrop, Warp, Crop, Translate, Resize, RandomCrop, Rotate90, RandomSizedCrop
+from .geometric import (
+  GeometricTransformation,
+  GeometricChain,
+  HorizontalFlip,
+  VerticalFlip,
+  Rotate,
+  CenterCrop,
+  Warp,
+  Crop,
+  Translate,
+  Resize,
+  RandomCrop,
+  Rotate90,
+  RandomSizedCrop,
+)
 
-from .colorspace import ColorspaceTransformation, ColorspaceChain, ByteToFloat, Normalize, ChannelShuffle, \
-        RandomGrayscale, RandomGamma, RandomBrightness, RandomContrast, ColorJitter, Solarization, RandomChannelGamma, \
-        ChannelDrop
+from .colorspace import (
+  ColorspaceTransformation,
+  ColorspaceChain,
+  ByteToFloat,
+  Normalize,
+  ChannelShuffle,
+  RandomGrayscale,
+  RandomGamma,
+  RandomBrightness,
+  RandomContrast,
+  ColorJitter,
+  Solarization,
+  RandomChannelGamma,
+  ChannelDrop,
+)
 
 from .imagelevel import GridShuffle, Blur, GaussianBlur
 
 from .optimized import Chain, OptimizedChain
 
+import importlib.metadata
+
+try:
+  __version__ = importlib.metadata.version(__name__)
+except importlib.metadata.PackageNotFoundError:
+  __version__ = "0.0.0"  # Fallback for development mode
